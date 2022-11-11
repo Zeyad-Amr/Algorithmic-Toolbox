c038863f1740f9356e068f2378330699db522eec
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2022-11-11 14:55:02 +0200

    add week 1

--- Week 1/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ Week 1/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -2,25 +2,30 @@
 #include <vector>
 #include <algorithm>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
+int MaxPairwiseProduct(const std::vector<int> &numbers)
+{
     int max_product = 0;
     int n = numbers.size();
 
-    for (int first = 0; first < n; ++first) {
-        for (int second = first + 1; second < n; ++second) {
+    for (int first = 0; first < n; ++first)
+    {
+        for (int second = first + 1; second < n; ++second)
+        {
             max_product = std::max(max_product,
-                numbers[first] * numbers[second]);
+                                   numbers[first] * numbers[second]);
         }
     }
 
     return max_product;
 }
 
-int main() {
+int main()
+{
     int n;
     std::cin >> n;
     std::vector<int> numbers(n);
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
         std::cin >> numbers[i];
     }
 
